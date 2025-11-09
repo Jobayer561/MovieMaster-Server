44a012b8f8e57838ee944682fec34ea2924fd7ed
jobayer561 <jobayer3085@gmail.com>
2025-11-09 11:46:01 +0600

    database connected and  movie collection added in database

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.oxmkd9u.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send(`Server is running on port : ${port}`);
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("movie-master");
+    const movieCollection = db.collection("movies");
+
+    app.get("/movies", async (req, res) => {
+      const result = await movieCollection.find().toArray();
+      res.send(result);
+    });
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
