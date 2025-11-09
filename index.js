df5301cd5aff37d47e9447de991cdee9ab1fa2f4
jobayer561 <jobayer3085@gmail.com>
2025-11-09 23:45:51 +0600

    movie detials api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -32,6 +32,14 @@ async function run() {
       const result = await movieCollection.find().toArray();
       res.send(result);
     });
+
+    app.get("/movies/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await movieCollection.findOne({ _id: new ObjectId(id) });
+      res.send({ success: true, result });
+    })
+
+    
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
