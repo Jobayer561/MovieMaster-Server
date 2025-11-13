296a45e97bebf3e3de7fb034219eab591c696378
jobayer561 <jobayer3085@gmail.com>
2025-11-13 20:09:09 +0600

    delete operation added

--- index.js
+++ index.js
@@ -88,30 +88,37 @@ async function run() {
       res.send(latestMovies);
     });
 
-    app.get('/myCollection',async(req,res)=>{
+    app.get("/myCollection", async (req, res) => {
       const email = req.query.email;
-        console.log("Fetching movies for:", email);
 
-      const movies = await movieCollection.find({addedBy : email}).toArray();
-      console.log('after finding email',movies);
-      res.send(movies)
-    })
+      const movies = await movieCollection.find({ addedBy: email }).toArray();
+      res.send(movies);
+    });
 
-        app.put("/movies/update/:id", async (req, res) => {
-          const { id } = req.params;
-          const data = req.body;
-          const objectId = new ObjectId(id);
-          const filter = { _id: objectId };
-          const update = {
-            $set: data,
-          };
-          const result = await movieCollection.updateOne(filter, update);
-          res.send({
-            success: true,
-            result,
-          });
-        });
-        
+    app.put("/movies/update/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+      const update = {
+        $set: data,
+      };
+      const result = await movieCollection.updateOne(filter, update);
+      res.send({
+        success: true,
+        result,
+      });
+    });
+    app.delete("/movies/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await movieCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send({
+        success: true,
+        result,
+      });
+    });
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
